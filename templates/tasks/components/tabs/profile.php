4a3546d3c66ea4aa9126bcdbf61f7f4996c09897
Davut <dsergeeb@smartdatatech.io>
2020-11-24 03:50:35 +0300

    Prototype profile (layouts), adaptive for profile

---
+++ templates/tasks/components/tabs/profile.php
@@ -0,0 +1,23 @@
+<div class="tab-profile">
+    <div class="row">
+        <div class="col-lg-3 col-md-4 col-sm-12 d-flex justify-content-center">
+            <div class="profile-img profile-img_borderer">
+                <div>
+                    <img src="/asset/img/1106631.svg" alt="profile-icon">
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-8 col-md-7 col-sm-12">
+            <h3 id="profileHeading">Profile</h3>
+            <form method="POST" id="profileSetting">
+                <div class="form-group">
+                    <input type="text" class="form-control" name="name" value="Name" disabled>
+                </div>
+                <div class="form-group">
+                    <input type="mail" class="form-control" name="login" value="example@mail.com" disabled>
+                </div>
+            </form>
+            <button class="btn btn-primary">Edit</button>
+        </div>
+    </div>
+</div>
